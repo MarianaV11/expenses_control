d6e90b508e60f8e6ed8dcd5950158dd9d2b8d735
Mari <marianavca002@outlook.com>
2025-08-09 23:05:21 -0300

    [User] Updating doc of route

--- backend/controller/user_controller.py
+++ backend/controller/user_controller.py
@@ -14,14 +14,20 @@ router = APIRouter()
 
 @router.post("/users/create")
 def create_user_route(user: UserCreate):
+    """Creates a new user"""
+
     return create_user(user)
 
 
 @router.post("/users/login", response_model=UserLoginReturn)
 def login_user_route(user: UserLogin):
+    """Return information of authenticated user"""
+
     return login(user)
 
 
 @router.get("/users/user", response_model=UserBase)
 def get_user_route(user_id: int, _: None = Depends(require_token_valid)):
+    """Return information of user by it's id"""
+
     return get_user(UserIdentifier(id=user_id))
