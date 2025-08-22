a9254d4fc2d416f40600e88b5cb6346f906d753c
mari <marianavieira@profectum.com.br>
2025-08-22 18:53:53 -0300

    docs(label): Updating info of label route

--- backend/controller/label_controller.py
+++ backend/controller/label_controller.py
@@ -20,11 +20,15 @@ router = APIRouter()
 
 @router.post("/labels/create", response_model=LabelRead)
 def create_label_route(label: LabelCreate, _: None = Depends(require_token_valid)):
+    """Create a new label for a user"""
+
     return create_label(label=label)
 
 
 @router.get("/labels/user_label", response_model=LabelRead)
 def get_label_route(label_id: int, _: None = Depends(require_token_valid)):
+    """Get a specific label by label id"""
+
     return get_label(label_id=LabelBase(id=label_id))
 
 
@@ -32,14 +36,20 @@ def get_label_route(label_id: int, _: None = Depends(require_token_valid)):
 def get_labels_route(
     user_id: int, page: int, per_page: int, _: None = Depends(require_token_valid)
 ):
+    """Get a label list of a user"""
+
     return get_labels(user_id=user_id, page=page, per_page=per_page)
 
 
 @router.put("/labels/update_label", response_model=LabelRead)
 def update_label_route(label_data: LabelUpdate, _: None = Depends(require_token_valid)):
+    """Update a label by label id"""
+
     return update_label(label_data=label_data)
 
 
 @router.delete("/labels/delete_label")
 def delete_label_route(label_id: LabelBase, _: None = Depends(require_token_valid)):
+    """Delete a label by label it"""
+
     return delete_label(label_id=label_id)
