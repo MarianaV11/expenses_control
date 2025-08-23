fc1d20385bd0d15bd225b6486066b4af08076d68
Mari <marianavca002@outlook.com>
2025-08-23 11:52:42 -0300

    perf(User): Refactor user service: update return on user creation, add rollback to functions, and improve exception handling

--- backend/controller/user_controller.py
+++ backend/controller/user_controller.py
@@ -20,7 +20,7 @@ from utils.auth import require_token_valid
 router = APIRouter()
 
 
-@router.post("/users/create")
+@router.post("/users/create", response_model=UserBase)
 def create_user_route(user: UserCreate):
     """Creates a new user"""
 
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -15,108 +15,134 @@ from utils.auth import create_access_token
 from utils.security import hash_password, verify_password
 
 
-def create_user(user_data: UserCreate) -> JSONResponse:
+def create_user(user_data: UserCreate) -> UserBase | JSONResponse:
     db = SessionLocal()
 
-    user = db.query(User).filter(User.email == user_data.email).first()
+    try:
+        user = db.query(User).filter(User.email == user_data.email).first()
 
-    if user:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail={"message": "Already a user created with this e-mail!"},
+        if user:
+            return JSONResponse(
+                status_code=status.HTTP_409_CONFLICT,
+                content={"message": "Already a user created with this e-mail!"},
+            )
+
+        new_user = User(
+            name=user_data.name,
+            email=user_data.email,
+            birthday=user_data.birthday,
+            is_restricted=user_data.is_restricted,
+            is_admin=user_data.is_admin,
+            password=hash_password(user_data.password),
         )
 
-    new_user = User(
-        name=user_data.name,
-        email=user_data.email,
-        birthday=user_data.birthday,
-        is_restricted=user_data.is_restricted,
-        is_admin=user_data.is_admin,
-        password=hash_password(user_data.password),
-    )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
 
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+        return new_user
+    except Exception as e:
+        db.rollback()
 
-    db.close()
-
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content={"message": "User created with success!", "id": new_user.id},
-    )
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occured when trying to create a user: {e}",
+        )
+    finally:
+        db.close()
 
 
-def login(user_data: UserLogin) -> UserLoginReturn:
+def login(user_data: UserLogin) -> UserLoginReturn | JSONResponse:
     db = SessionLocal()
 
-    user = db.query(User).filter(User.email == user_data.email).first()
+    try:
+        user = db.query(User).filter(User.email == user_data.email).first()
 
-    if not user or not verify_password(
-        plain_password=user_data.password, hashed_password=user.password
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail={"message": "Wrong password or e-mail!"},
-            headers={"WWW-Authenticate": "Bearer"},
+        if not user or not verify_password(
+            plain_password=user_data.password, hashed_password=user.password
+        ):
+            return JSONResponse(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={"message": "Wrong password or e-mail!"},
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        token = create_access_token(data={"sub": user.email})
+
+        return UserLoginReturn(
+            auth=UserAuth(access_token=token, token_type="bearer"), id=user.id
         )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occured when trying to auth: {e}",
+        )
+    finally:
+        db.close()
 
-    token = create_access_token(data={"sub": user.email})
 
-    db.close()
-
-    return UserLoginReturn(
-        auth=UserAuth(access_token=token, token_type="bearer"), id=user.id
-    )
-
-
-def get_user(user_id: UserIdentifier) -> UserBase:
+def get_user(user_id: UserIdentifier) -> UserBase | JSONResponse:
     db = SessionLocal()
 
-    user = db.query(User).filter(User.id == user_id.id).first()
+    try:
+        user = db.query(User).filter(User.id == user_id.id).first()
 
-    if not user:
+        if not user:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={"message": "Current user not found in our database."},
+            )
+
+        return UserBase.model_validate(user)
+    except Exception as e:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail={"message": "Current user not found in our database."},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occured when trying to get an user: {e}",
         )
-
-    db.close()
-
-    return UserBase.model_validate(user)
+    finally:
+        db.close()
 
 
 def delete_user(user_id: UserIdentifier) -> JSONResponse:
     db = SessionLocal()
 
-    user = db.query(User).filter(User.id == user_id.id).first()
+    try:
+        user = db.query(User).filter(User.id == user_id.id).first()
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail={"message": "Current user not found in our database."},
+        if not user:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={"message": "User not found in our database."},
+            )
+
+        db.delete(user)
+        db.commit()
+
+        return JSONResponse(
+            content={"message": "User deleted successfully!"},
+            status_code=status.HTTP_200_OK,
         )
+    except Exception as e:
+        db.rollback()
 
-    db.delete(user)
-    db.commit()
-    db.close()
-
-    return JSONResponse(
-        content={"message": "User deleted successfully!"},
-        status_code=status.HTTP_200_OK,
-    )
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occured when trying to delete an user: {e}",
+        )
+    finally:
+        db.close()
 
 
-def update_user(user_data: UserBase) -> UserBase:
+def update_user(user_data: UserBase) -> UserBase | JSONResponse:
     db = SessionLocal()
 
     try:
         user = db.query(User).filter(User.id == user_data.id).first()
 
         if not user:
-            raise HTTPException(
+            return JSONResponse(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail={"message": "Current user not found in our database."},
+                content={"message": "Current user not found in our database."},
             )
 
         user.name = user_data.name
@@ -128,11 +154,13 @@ def update_user(user_data: UserBase) -> UserBase:
         db.commit()
         db.refresh(user)
 
-        return UserBase.model_validate(user)
+        return user
     except Exception as e:
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={"message": f"An error occurred: {e}"},
+        db.rollback()
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occured when trying to update an user: {e}",
         )
     finally:
         db.close()
@@ -145,16 +173,16 @@ def update_user_password(user_data: UserUpdatePassword) -> JSONResponse:
         user = db.query(User).filter(User.id == user_data.id).first()
 
         if not user:
-            raise HTTPException(
+            return JSONResponse(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail={"message": "Current user not found in our database."},
+                content={"message": "Current user not found in our database."},
             )
         if not verify_password(
             plain_password=user_data.old_password, hashed_password=user.password
         ):
-            raise HTTPException(
+            return JSONResponse(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail={"message": "The old password is wrong."},
+                content={"message": "The old password is wrong."},
             )
 
         user.password = hash_password(user_data.new_password)
@@ -162,11 +190,16 @@ def update_user_password(user_data: UserUpdatePassword) -> JSONResponse:
         db.commit()
         db.refresh(user)
 
-        return JSONResponse(content={"message": "Password updated successfully!"})
-    except Exception as e:
         return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={"message": f"An error occurred: {e}"},
+            content={"message": "Password updated successfully!"},
+            status_code=status.HTTP_200_OK,
+        )
+    except Exception as e:
+        db.rollback()
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occured when trying to update an user password: {e}",
         )
     finally:
         db.close()
