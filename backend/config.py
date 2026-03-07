77f6436b28f9d5c938491a4aa8a9d4d90dae906e
Mari <marianavca002@outlook.com>
2026-03-07 15:01:33 -0300

    Updates backend to support Dockerized PostgreSQL

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URL = (
-        "postgresql://postgres:postgres@localhost:5432/expenses_control"
-    )
+    SQLALCHEMY_DATABASE_URL = "postgresql://user:postgres@db:5432/expenses_control"
     SECRET_KEY = "a54d88e06612d820bc3be72877c74f257b561b19ac03fae09a790f9f4fdc5bd8"
     ALGORITHM = "HS256"
--- frontend/service/axios_config.ts
+++ frontend/service/axios_config.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 import { getToken } from "./local_storage";
 
 export const axios = Axios.create({
-  baseURL: "http://127.0.0.1:9000/api/",
+  baseURL: "http://localhost:8000/api/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
